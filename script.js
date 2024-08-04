fbab7cc8c1a7ceee5a47e4ebe2d4fab229689faf
gohar-anjum <goharanjum49@gmail.com>
2024-08-05 00:15:02 +0500

    A done for C

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 let main = document.querySelector(".main");
 let cursor = document.querySelector("#cursor");
+let imagediv = document.querySelector("#image")
 
 main.addEventListener("mousemove", function(dets){
     gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y
+    })
+})
+imagediv.addEventListener("mouseenter", function(dets){
+    gsap.to(cursor,{
+        scale:2,
+        x:dets.x,
+        y:dets.y
+    })
+})
+imagediv.addEventListener("mouseleave", function(dets){
+    gsap.to(cursor,{
+        scale:1,
         x:dets.x,
         y:dets.y
     })
