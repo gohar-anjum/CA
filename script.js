79fb4655435a8bca5666ac900820945592bbd3e2
gohar-anjum <goharanjum49@gmail.com>
2024-08-04 23:37:05 +0500

    A done for C

---
+++ script.js
@@ -0,0 +1,9 @@
+let main = document.querySelector(".main");
+let cursor = document.querySelector("#cursor");
+
+main.addEventListener("mousemove", function(dets){
+    gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y
+    })
+})
